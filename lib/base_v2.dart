45420e5fb004ffff3c40369f2d2e9f4258f82b71
Trần Đức <44558894+tranduc1710@users.noreply.github.com>
2025-02-01 01:42:41 +0700

    version 1

--- lib/base_v2.dart
+++ lib/base_v2.dart
@@ -1,10 +1,2 @@
-import 'base_v2_platform_interface.dart';
-
 export 'extends/extensions.dart';
 export 'extends/inherited.dart';
-
-class BaseV2 {
-  Future<String?> getPlatformVersion() {
-    return BaseV2Platform.instance.getPlatformVersion();
-  }
-}
